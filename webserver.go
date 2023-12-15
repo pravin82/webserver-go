bcc76e024daba0cc0b82819e33f3a9871be136ac
pravin82 <pravinmishra1796@gmail.com>
2023-12-15 21:07:49 +0530

    Create the basic webserver

--- webserver.go
+++ webserver.go
@@ -1,34 +1,117 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", servePage)
-	http.ListenAndServe(":8080", nil)
+	listenAddr := "127.0.0.1:7878"
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		fmt.Println("Error Creating Listener")
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Listening on ", listenAddr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error Accepting connection", err)
+			continue
+		}
+		handleConnection(conn)
+
+	}
 
 }
 
-func servePage(writer http.ResponseWriter, request *http.Request) {
-	urlPath := request.URL.Path
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Accepted Connection from:", conn.RemoteAddr())
+	reader := bufio.NewReader(conn)
+
+	request, err := readHTTPRequest(reader)
+	if err != nil {
+		fmt.Println("Error reading from Connection", err)
+		return
+	}
+	statusLine := "HTTP/1.1 200 OK"
+	filePath := "index.html"
+	path := extractPath(request)
+	if path != "GET / HTTP/1.1\r" {
+		statusLine = "HTTP/1.1 404 NOT FOUND"
+		filePath = "404.html"
+	}
+	fmt.Println("Path of request ", path)
+	fileContent, err := readFile(filePath)
+	if err != nil {
+		fmt.Println("Error opening file")
+	}
+	fileContentLength := len(fileContent)
+	response := fmt.Sprintf("%s\r\nContent-Length: %d\r\n\r\n%s", statusLine, fileContentLength, fileContent)
+	conn.Write([]byte(response))
+
+}
+func extractPath(request string) string {
+	lines := strings.Split(request, "\n")
+
+	firstLine := lines[0]
+
+	//fields := strings.Fields(firstLine)
+
+	return firstLine
+}
+
+func readHTTPRequest(reader *bufio.Reader) (string, error) {
+	var requestLines []string
+
+	// Read lines until an empty line is encountered (end of headers)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+		if line == "\r\n" || line == "\n" {
+			break
+		}
+		requestLines = append(requestLines, line)
+	}
+
+	// Combine the request lines into a single string
+	return strings.Join(requestLines, ""), nil
+}
+
+func sleep(writer http.ResponseWriter, request *http.Request) {
+	duration := 15 * time.Second
+	time.Sleep(duration)
+	io.WriteString(writer, "Response after sleep")
+}
+
+func readFile(urlPath string) (string, error) {
 	if urlPath == "" {
 		urlPath = "index.html"
 	}
 	filePath := "www/" + urlPath
 	file, err := os.Open(filePath)
 	if err != nil {
-		http.Error(writer, "Not Found", http.StatusNotFound)
-		return
+		return "", err
+	}
+	scanner := bufio.NewScanner(file)
+	var content string
+	for scanner.Scan() {
+		content += scanner.Text() + "\n"
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
 	}
 	defer file.Close()
-	writer.Header().Set("Content-Type", "text/html")
-	_, err = io.Copy(writer, file)
-	if err != nil {
-		http.Error(writer, "Error copying file contents", http.StatusInternalServerError)
-		return
-	}
+	return content, nil
 
 }
